c0b2dc559081d1ba3f5c1837fc21c886fe9bb8cb
Gabriel Hildebrandt Pereira <ghp010203@gmail.com>
2024-01-02 12:49:57 -0300

    ta diferente

--- js/coisas/logoHover.js
+++ js/coisas/logoHover.js
@@ -1,1 +1,38 @@
-const img = window.document.get
+const text = window.document.getElementById("text");
+const img = window.document.getElementById("img");
+
+text.style.opacity = 0;
+
+img.addEventListener("click", clicou)
+
+img.style.transform="translateY(92px)"; 
+
+let click = -1;
+function clicou() {
+    click++
+    if(click == 0){
+        img.style.transform="translateY(30px)";
+        text.style.opacity = 1;
+    }
+    if(click == 1){
+        img.style.transform="translateY(92px)";
+        text.style.opacity = 0;
+        click = -1;
+    }
+}
+
+img.addEventListener("mouseenter", foi);
+let passou = -1;
+
+function foi() {
+    passou++
+    if(passou == 0){
+        img.style.transform="translateY(30px)";
+        text.style.opacity = 1;
+    }
+    if(passou == 1){
+        img.style.transform="translateY(92px)";
+        text.style.opacity = 0;
+        passou = -1;
+    }
+}
